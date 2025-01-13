4bac3b813ecaa7bf5f59092902c3322692674a96
dharmar <dharmaraws@gmail.com>
2025-01-13 18:01:08 +0530

    installed React and Tailwind CSS

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-green-800">
+        Hello, I am learning MERN from Dr.Sahand
+      </h1>
+    </div>
+  );
+};
+
+export default App;
