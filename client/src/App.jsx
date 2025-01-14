9ea05469a1e2e008c4f97c8b9cb36cfc114bb2d7
dharmar <dharmaraws@gmail.com>
2025-01-14 11:02:08 +0530

    create pages and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Products from "./pages/Products.jsx";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl text-green-800">
-        Hello, I am learning MERN from Dr.Sahand
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
